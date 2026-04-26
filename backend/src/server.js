f29a2b7503f138f5eb1969f1e56653192929faa0
Jinny112477 <pareenaginnie@gmail.com>
2026-04-26 20:10:45 +0700

    edit 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,8 +20,12 @@ const server = http.createServer(app);
 // WEBSOCKET
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ],
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
